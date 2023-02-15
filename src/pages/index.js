c1c35fb9666c006a6008ba4a460d03efc95843de
Eslim Daga <eslimdaga@gmail.com>
2023-02-15 14:10:45 -0500

    feat:📝:Agregué la imagen de fondo para poner un gradiente personalizado

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,11 +5,14 @@ export default function Home() {
 	return (
 		<div>
 			<Head>
-				<title>Peliculas con emojis 🥳🎉</title>
+				<title>Peliculas con emojis 🥳</title>
 			</Head>
 			<div className="w-screen h-screen">
-				{/* Two cards */}
-				<div className="flex">Peliculas con emojis</div>
+				<img
+					src="https://image.tmdb.org/t/p/original/xDMIl84Qo5Tsu62c9DGWhmPI67A.jpg"
+					alt="my-big-fat-greek-wedding-2"
+					className="w-full h-full object-cover"
+				/>
 			</div>
 		</div>
 	);
