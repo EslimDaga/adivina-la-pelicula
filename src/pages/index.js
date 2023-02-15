6250c77bddbc9149d968c5e7d21439bbfdefc0bf
Eslim Daga <eslimdaga@gmail.com>
2023-02-15 16:00:11 -0500

    feat:📝:La imagen ahora cambia según la proporción de la pantalla, tambien eh agregado un degradado para que la typografía se note mejor.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,30 @@
 import Head from "next/head";
-import { useTheme } from "next-themes";
+import styled from "styled-components";
+
+const BackgroundImage = styled.div`
+	background-repeat: no-repeat;
+	background-size: cover;
+	background-position: center;
+	height: 100vh;
+	background-image: url("https://image.tmdb.org/t/p/original/9Rq14Eyrf7Tu1xk0Pl7VcNbNh1n.jpg");
+	@media screen and (max-width: 768px) {
+		background-image: url("https://image.tmdb.org/t/p/original/juoinefK6tMbjwJhRpRvbAAmrTB.jpg");
+	}
+`;
+
+const ImageGradient = styled.div`
+	position: absolute;
+	background: linear-gradient(
+		180deg,
+		rgba(0, 0, 0, 0.8) 100%,
+		rgba(0, 0, 0, 0.8) 100%
+	);
+	height: 100vh;
+	top: 0;
+	left: 0;
+	right: 0;
+	bottom: 0;
+`;
 
 export default function Home() {
 	return (
@@ -7,13 +32,8 @@ export default function Home() {
 			<Head>
 				<title>Peliculas con emojis 🥳</title>
 			</Head>
-			<div className="w-screen h-screen">
-				<img
-					src="https://image.tmdb.org/t/p/original/xDMIl84Qo5Tsu62c9DGWhmPI67A.jpg"
-					alt="my-big-fat-greek-wedding-2"
-					className="w-full h-full object-cover"
-				/>
-			</div>
+			<BackgroundImage />
+			<ImageGradient />
 		</div>
 	);
 }
