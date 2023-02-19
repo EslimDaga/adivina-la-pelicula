4f4a83b51f9688e7bbf010aee8e97fb57497d522
Eslim Daga <esliim9811@gmail.com>
2023-02-19 18:27:54 -0500

    feat:📝:Ahora ya puede generar en base al título de la película, emojis. También botones para poder ver el resultado y cambiar de película.

--- src/pages/movie.js
+++ src/pages/movie.js
@@ -10,11 +10,15 @@ import "react-lazy-load-image-component/src/effects/blur.css";
 
 const url_image = "https://image.tmdb.org/t/p/original";
 const url_movies_by_gender = "https://api.themoviedb.org/3/discover/movie";
+const url_open_ai = "https://api.openai.com/v1/completions";
 
 const Movie = () => {
 	const router = useRouter();
 	const [movie, setMovie] = useState({});
 	const [loading, setLoading] = useState(true);
+	const [loadingOpenAi, setLoadingOpenAi] = useState(false);
+	const [showTitle, setShowTitle] = useState(false);
+	const [emoji, setEmoji] = useState("");
 
 	const { gender_id } = router.query;
 
@@ -38,6 +42,47 @@ const Movie = () => {
 		});
 	}, []);
 
+	const handleConvertToEmoji = title => {
+		setLoadingOpenAi(true);
+		axios
+			.post(
+				url_open_ai,
+				{
+					model: "text-davinci-002",
+					prompt: `Convert the movie ${title} to emojis, only respond with emojis`,
+					temperature: 0.5,
+					stop: "none",
+					max_tokens: 1024,
+				},
+				{
+					headers: {
+						"Content-Type": "application/json",
+						Authorization:
+							"Bearer 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",
+					},
+				}
+			)
+			.then(response => {
+				const title_convert = response.data.choices[0].text;
+
+				if (title_convert.length > 10) {
+					setEmoji(title_convert.slice(0, 10));
+				} else {
+					setEmoji(title_convert);
+				}
+
+				setLoadingOpenAi(false);
+			});
+	};
+
+	const handleNextMovie = () => {
+		router.reload();
+	};
+
+	const handleShowTitle = () => {
+		setShowTitle(true);
+	};
+
 	return (
 		<div className="bg-yellow-500 w-screen h-screen px-4">
 			<Head>
@@ -53,18 +98,47 @@ const Movie = () => {
 				</button>
 			</Link>
 
-			<div className="flex flex-col gap-5 items-center justify-center h-full">
+			<div className="flex flex-col gap-8 items-center justify-center h-full">
 				{loading ? (
 					<div className="w-screen h-screen flex items-center justify-center">
 						<InfinitySpin width="200" color="#FFFFFF" />
 					</div>
 				) : movie.backdrop_path ? (
-					<LazyLoadImage
-						effect="blur"
-						src={`${url_image}${movie.backdrop_path}`}
-						alt={`${url_image}${movie.backdrop_path}`}
-						className="md:max-w-2xl lg:max-w-4xl rounded-xl shadow-3xl"
-					/>
+					<>
+						<LazyLoadImage
+							effect="blur"
+							src={`${url_image}${movie.backdrop_path}`}
+							alt={`${url_image}${movie.backdrop_path}`}
+							className="md:max-w-2xl lg:max-w-4xl rounded-xl shadow-3xl"
+						/>
+						<button
+							onClick={() => handleConvertToEmoji(movie.original_title)}
+							disabled={loadingOpenAi}
+							className={`px-4 py-4 bg-white hover:bg-gray-700 text-gray-900 hover:text-white font-semibold rounded-xl ${
+								loadingOpenAi && "cursor-not-allowed"
+							}`}
+						>
+							{loadingOpenAi ? "🤖 Convirtiendo..." : "🚀 Convertir a emojis"}
+						</button>
+						{emoji && <p className="text-gray-900 text-4xl">{emoji}</p>}
+						{showTitle && (
+							<p className="text-gray-900 font-semibold text-3xl">
+								{movie.title}
+							</p>
+						)}
+						<button
+							onClick={handleNextMovie}
+							className="px-4 py-4 bg-white hover:bg-gray-700 text-gray-900 hover:text-white font-semibold rounded-xl"
+						>
+							Siguiente película
+						</button>
+						<button
+							onClick={handleShowTitle}
+							className="px-4 py-4 bg-white hover:bg-gray-700 text-gray-900 hover:text-white font-semibold rounded-xl"
+						>
+							Ver resultado
+						</button>
+					</>
 				) : (
 					<>
 						<LazyLoadImage
