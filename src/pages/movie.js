6e60d57938a5a7950e4596ba378ccc5cac053206
Eslim Daga <eslimdaga@gmail.com>
2023-02-21 15:25:27 -0500

    feat:📝:Del api de The Movie DB ahora se generan 3 posibles alternativas y la respuesta correcta

--- src/pages/movie.js
+++ src/pages/movie.js
@@ -15,6 +15,7 @@ const url_open_ai = "https://api.openai.com/v1/completions";
 const Movie = () => {
 	const router = useRouter();
 	const [movie, setMovie] = useState({});
+	const [posibleMovies, setPosibleMovies] = useState([]);
 	const [loading, setLoading] = useState(true);
 
 	const { gender_id } = router.query;
@@ -43,6 +44,20 @@ const Movie = () => {
 					randomMovie.backdrop_path &&
 					randomMovie.original_language === "en"
 				) {
+					const posibleMovies = response.data.results
+						.filter(
+							movie =>
+								movie.id !== randomMovie.id && movie.original_language === "en"
+						)
+						.sort(() => Math.random() - 0.5)
+						.slice(0, 3);
+
+					//Add the random movie to the posible movies and shuffle the array
+					posibleMovies.push(randomMovie);
+					posibleMovies.sort(() => Math.random() - 0.5);
+
+					setPosibleMovies(posibleMovies);
+
 					setMovie(randomMovie);
 				}
 				setLoading(false);
@@ -70,7 +85,7 @@ const Movie = () => {
 				href="/"
 				className="absolute max-w-max max-h-max top-4 left-4 lg:bottom-8 lg:right-8 z-50"
 			>
-				<button className="bg-white hover:bg-gray-700 text-gray-900 hover:text-white font-semibold text-sm py-4 px-4 rounded-xl flex gap-2 items-center">
+				<button className="bg-white text-gray-900 font-semibold text-sm py-4 px-4 rounded-xl flex gap-2 items-center">
 					<ArrowLeftIcon className="h-6 w-6" /> Volver
 				</button>
 			</Link>
@@ -82,7 +97,7 @@ const Movie = () => {
 				<FilmIcon className="h-6 w-6" /> Siguiente película
 			</button>
 
-			<div className="flex flex-col items-center justify-center h-screen">
+			<div className="flex flex-col gap-14 items-center justify-center h-screen">
 				{loading ? (
 					<div className="w-screen h-screen flex items-center justify-center">
 						<InfinitySpin width="200" color="#FFFFFF" />
@@ -95,6 +110,16 @@ const Movie = () => {
 							alt={`${url_image}${movie.backdrop_path}`}
 							className="md:max-w-2xl lg:max-w-4xl rounded-xl shadow-3xl"
 						/>
+						<div className="flex flex-col lg:flex-row gap-3 lg:gap-7">
+							{posibleMovies.map(movie => (
+								<button
+									key={movie.id}
+									className="bg-white text-gray-900 font-semibold text-sm px-4 py-4 rounded-xl shadow-3xl"
+								>
+									{movie.title}
+								</button>
+							))}
+						</div>
 					</>
 				) : (
 					<>
